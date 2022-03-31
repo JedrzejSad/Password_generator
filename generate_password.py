b9e83564dd29f03e443b68c05cdc43375a585780
JedrzejSad <jedrzejsadlon@gmail.com>
2022-03-31 11:42:10 +0200

    final

--- generate_password.py
+++ generate_password.py
@@ -4,16 +4,16 @@ import string
 string_with_all_characters = list(string.ascii_letters + string.digits + string.punctuation)
 
 def generate_password():
-    lenght=str(input("Wprowadź długość hasła: "))
+    lenght=str(input("How many signs in password You want: "))
     while not lenght.isdigit():
-        lenght = input("Wpisz liczbę: ")
+        lenght = input("Only digits required ")
     random.shuffle(string_with_all_characters)
     password=[]
     for i in range(int(lenght)):
         password.append(random.choice(string_with_all_characters))
         
     random.shuffle(password)
-    print("Twoje hasło to:")
+    print("Your password:")
     print("" .join(password))
         
 
